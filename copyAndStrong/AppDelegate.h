75f8e4e72758ad43021384933969e15d7c47609f
小河 <2459307230@qq.com>
2017-05-27 14:30:13 +0800

    Initial Commit

---
+++ copyAndStrong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  copyAndStrong
+//
+//  Created by 小河 on 2017/5/27.
+//  Copyright © 2017年 小河. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ copyAndStrongTests/copyAndStrongTests.m
@@ -0,0 +1,39 @@
+//
+//  copyAndStrongTests.m
+//  copyAndStrongTests
+//
+//  Created by 小河 on 2017/5/27.
+//  Copyright © 2017年 小河. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface copyAndStrongTests : XCTestCase
+
+@end
+
+@implementation copyAndStrongTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
